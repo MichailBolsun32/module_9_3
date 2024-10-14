8fdcdf74d985f211b52ceded8c3df216cb8c2f55
MichailBolsun32 <bolsun32@mail.ru>
2024-10-15 00:09:02 +0300

    Add files via upload

---
+++ module_9_3.py
@@ -0,0 +1,26 @@
+# Дано 2 списка:
+
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
+
+# Необходимо создать 2 генераторных сборки:
+
+# В переменную first_result запишите генераторную сборку,
+# которая высчитывает разницу длин строк из списков first и second, если их длины не равны.
+# Для перебора строк попарно из двух списков используйте функцию zip.
+
+first_result = ((len(x[0]) - len(x[1])) for x in zip(first, second) if len(x[0]) != len(x[1]))
+# объединяе два list в list tuple (zip(first, second)),
+# на каждом шаге итерации производим вычисления (len(x[0]) - len(x[1])
+
+
+#В переменную second_result запишите генераторную сборку,
+# которая содержит результаты сравнения длин строк в одинаковых позициях из списков first и second.
+# Составьте эту сборку НЕ используя функцию zip. Используйте функции range и len.
+
+second_result = ((len(first[index]) == len(second[index])) for index in range(len(first)))
+# генерируем цикл на range(len(first)) итераций,
+# затем сравниваем (len(first[index]) == len(second[index])
+
+print(list(first_result))
+print(list(second_result))
